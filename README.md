280348930ce85dacfeb0f13ea90fecdfcf6f7841
xiehuaqiang <xiehuaqiang@xiaojubianli.com>
2020-08-04 22:01:27 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,5 @@
+### 第一步
+
+- 全局安装typescript  `npm install typescript -g`
+- 创建tsconfig.js文件 `tsc --init`
+- 安装webpack `npm install webpack webpack-cli webpack-dev-server -D`
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Description: 
+ * @Author: xiehuaqiang
+ * @Date: 2020-08-04 21:09:24
+ */ 
+
+let hello : string = 'hello TypeScript'
+
+document.querySelectorAll('.app')[0].innerHTML = hello
